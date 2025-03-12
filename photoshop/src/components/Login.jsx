234afd43ac7fc782716e9624095e2b93bd34112c
ManishPrakkash <manishprakkash@gmail.com>
2025-03-12 14:03:58 +0000

    login page added

---
+++ photoshop/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../App.css";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-left">
+        <div className="login-form">
+          <h1>Branch Login</h1>
+          <p>Enter your email and password to log in!</p>
+          <select>
+            <option>Select your Branch...</option>
+          </select>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#" className="forgot-password">Forgot password?</a>
+          <div className="keep-logged-in">
+            <input type="checkbox" id="keep-logged-in" />
+            <label htmlFor="keep-logged-in">Keep me logged in</label>
+          </div>
+          <button>Login</button>
+        </div>
+      </div>
+      <div className="login-right">
+        <img src="photoshop/src/assets/Logo.png" alt="Logo" />
+        <h1>NAME</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
